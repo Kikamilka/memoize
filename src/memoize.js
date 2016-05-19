6850904b7c0e5b67f0251f7ac4e7ad33c15cbe44
Kamila Zaeva <Kamila_Zaeva@epam.com>
2016-05-19 12:32:10 +0300

    implement memoize function

--- src/memoize.js
+++ src/memoize.js
@@ -1,5 +1,9 @@
 export { memoize };
-
 function memoize(func) {
-	return func; // TODO
+	var result = {};
+	return function () {
+		var args = JSON.stringify(arguments);
+		if (!result.hasOwnProperty(args)) result[args] = func.apply(null, arguments);
+		return result[args];
+	};
 }
